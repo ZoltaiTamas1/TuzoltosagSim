fe5835773071cc216dd211cb12f885af66414609
ZoltaiTamas1 <zoltai.tamas@students.jedlik.eu>
2025-02-12 10:11:48 +0100

    vizforras.cs kezdes

--- Vizforras.cs
+++ Vizforras.cs
@@ -2,5 +2,9 @@
 
 namespace TuzoltosagSim
 {
-    
+    public class Vizforras
+    {
+        public string Hely { get; private set; }
+        public int VizMennyiseg { get; private set; }
+    }
 }
