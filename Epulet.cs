201cf1dc8071dd233b22e0d73b2e29bf6c030ef5
ZoltaiTamas1 <zoltai.tamas@students.jedlik.eu>
2025-02-12 10:10:58 +0100

    epulet.cs kesz

--- Epulet.cs
+++ Epulet.cs
@@ -8,5 +8,30 @@ namespace TuzoltosagSim
         public bool TuzVan { get; private set; }
         public TuzTipus TuzTipus { get; private set; }
         public BuildingType Tipus { get; private set; }
+
+        public Epulet(string cim, BuildingType tipus)
+        {
+            Cim = cim;
+            Tipus = tipus;
+            TuzVan = false;
+            TuzTipus = TuzTipus.Közönséges;
+        }
+
+        public void TuzKiindul(TuzTipus tuzTipus)
+        {
+            if (TuzVan)
+                throw new Exception($"A(z) {Cim} címen már van tűz.");
+
+            TuzVan = true;
+            TuzTipus = tuzTipus;
+        }
+
+        public void TuzEloltva()
+        {
+            if (!TuzVan)
+                throw new Exception($"A(z) {Cim} címen nincs tűz.");
+
+            TuzVan = false;
+        }
     }
 }
