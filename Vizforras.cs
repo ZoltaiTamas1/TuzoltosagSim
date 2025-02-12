abf1e662a19bfec5b6cf6d3822415e4c2fb4971b
ZoltaiTamas1 <zoltai.tamas@students.jedlik.eu>
2025-02-12 10:12:12 +0100

    vizforras.cs kesz

--- Vizforras.cs
+++ Vizforras.cs
@@ -6,5 +6,19 @@ namespace TuzoltosagSim
     {
         public string Hely { get; private set; }
         public int VizMennyiseg { get; private set; }
+
+        public Vizforras(string hely, int vizMennyiseg)
+        {
+            Hely = hely;
+            VizMennyiseg = vizMennyiseg;
+        }
+
+        public void VizetHasznal(int mennyiseg)
+        {
+            if (VizMennyiseg < mennyiseg)
+                throw new Exception($"Nincs elég víz a(z) {Hely} helyen.");
+
+            VizMennyiseg -= mennyiseg;
+        }
     }
 }
