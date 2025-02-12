ab472d87ed34db371e512af03c1649e492e238e0
ZoltaiTamas1 <zoltai.tamas@students.jedlik.eu>
2025-02-12 10:13:35 +0100

    tuzoltoauto.cs kesz

--- Tuzoltoauto.cs
+++ Tuzoltoauto.cs
@@ -6,5 +6,24 @@ namespace TuzoltosagSim
     {
         public string Rendszam { get; private set; }
         public bool Szabad { get; private set; }
+
+        public Tuzoltoauto(string rendszam)
+        {
+            Rendszam = rendszam;
+            Szabad = true;
+        }
+
+        public void Kivonul(Tuzolto tuzolto, Epulet epulet)
+        {
+            if (!Szabad)
+                throw new Exception($"{Rendszam} rendszámú autó már foglalt.");
+
+            Szabad = false;
+        }
+
+        public void Visszaérkezik()
+        {
+            Szabad = true;
+        }
     }
 }
