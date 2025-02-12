ada09e34cad9e575a517dc0c0a414c3774aece06
ZoltaiTamas1 <zoltai.tamas@students.jedlik.eu>
2025-02-12 10:14:52 +0100

    varos.cs kezdes

--- Varos.cs
+++ Varos.cs
@@ -4,5 +4,11 @@ using System.Linq;
 
 namespace TuzoltosagSim
 {
-    
+    public class Varos
+    {
+        public List<Epulet> Epuletek { get; private set; }
+        public List<Tuzolto> TuzoltoK { get; private set; }
+        public List<Tuzoltoauto> TuzoltoAutok { get; private set; }
+        public List<Vizforras> Vizforrasok { get; private set; }
+    }
 }
