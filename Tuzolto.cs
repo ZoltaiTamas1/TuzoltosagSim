23299f540013066b5bce47ad71933f667e4bd623
ZoltaiTamas1 <zoltai.tamas@students.jedlik.eu>
2025-02-12 10:14:14 +0100

    tuzolto.cs kesz

--- Tuzolto.cs
+++ Tuzolto.cs
@@ -6,5 +6,28 @@ namespace TuzoltosagSim
     {
         public string Nev { get; private set; }
         public bool Szabad { get; private set; }
+
+        public Tuzolto(string nev)
+        {
+            Nev = nev;
+            Szabad = true;
+        }
+
+        public void Riasztas(Epulet epulet)
+        {
+            if (!Szabad)
+                throw new Exception($"{Nev} már foglalt, nem tud riasztásra reagálni.");
+
+            Szabad = false;
+        }
+
+        public void Tuzoltas(Epulet epulet)
+        {
+            if (Szabad)
+                throw new Exception($"{Nev} szabad, nem tud tüzet oltani.");
+
+            epulet.TuzEloltva();
+            Szabad = true;
+        }
     }
 }
