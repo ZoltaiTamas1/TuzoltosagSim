fc093644f64069c771c91fea7b65278124bde632
ZoltaiTamas1 <zoltai.tamas@students.jedlik.eu>
2025-02-12 10:07:54 +0100

    epulet.cs alapok

--- Epulet.cs
+++ Epulet.cs
@@ -2,5 +2,11 @@
 
 namespace TuzoltosagSim
 {
-   
+    public class Epulet
+    {
+        public string Cim { get; private set; }
+        public bool TuzVan { get; private set; }
+        public TuzTipus TuzTipus { get; private set; }
+        public BuildingType Tipus { get; private set; }
+    }
 }
