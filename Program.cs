b63fc04ef5914e43373e77e867819198208b9341
[Zoltai Tamás] <[zoltai.tamas@students.jedlik.eu]>
2025-02-17 16:52:49 +0100

    haz_leeges

--- Program.cs
+++ Program.cs
@@ -14,16 +14,14 @@ namespace TuzoltosagSim
             while (futas)
             {
                 Console.Clear();
-                Console.WriteLine("=== Tűzoltó Szimulátor ===");
+                KirajzolFejlec("=== Tűzoltó Szimulátor ===");
                 Console.WriteLine("1. Épület hozzáadása");
                 Console.WriteLine("2. Tűzoltó hozzáadása");
                 Console.WriteLine("3. Tűzoltóautó hozzáadása");
                 Console.WriteLine("4. Vízkút hozzáadása");
-                Console.WriteLine("5. Tűz indítása");
-                Console.WriteLine("6. Tűz oltása");
-                Console.WriteLine("7. Szimuláció indítása");
-                Console.WriteLine("8. Kilépés");
-                Console.Write("Válassz egy opciót: ");
+                Console.WriteLine("5. Szimuláció indítása");
+                Console.WriteLine("6. Kilépés");
+                Console.Write("\nVálassz egy opciót: ");
 
                 string valasztas = Console.ReadLine()!;
 
@@ -42,28 +40,47 @@ namespace TuzoltosagSim
                         VizforrasHozzaad(varos);
                         break;
                     case "5":
-                        TuzInditas(varos);
-                        break;
-                    case "6":
-                        TuzOltas(varos);
-                        break;
-                    case "7":
                         SzimulacioInditasa(varos);
                         break;
-                    case "8":
+                    case "6":
                         futas = false;
                         break;
                     default:
-                        Console.WriteLine("Érvénytelen választás. Nyomj egy gombot az újrapróbálkozáshoz.");
+                        HibaKiirasa("Érvénytelen választás. Nyomj egy gombot az újrapróbálkozáshoz.");
                         Console.ReadKey();
                         break;
                 }
             }
         }
 
+        static void KirajzolFejlec(string cim)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(new string('=', cim.Length));
+            Console.WriteLine(cim);
+            Console.WriteLine(new string('=', cim.Length));
+            Console.ResetColor();
+            Console.WriteLine();
+        }
+
+        static void HibaKiirasa(string uzenet)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(uzenet);
+            Console.ResetColor();
+        }
+
+        static void SikerKiirasa(string uzenet)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(uzenet);
+            Console.ResetColor();
+        }
+
         static void EpuletHozzaad(Varos varos)
         {
-            Console.WriteLine("--------------------------------------------------\n");
+            Console.Clear();
+            KirajzolFejlec("=== Épület Hozzáadása ===");
             Console.Write("Épület címe: ");
             string cim = Console.ReadLine()!;
 
@@ -85,48 +102,50 @@ namespace TuzoltosagSim
                 }
                 catch (Exception)
                 {
-                    Console.WriteLine("Érvénytelen típus. Add meg újra!");
+                    HibaKiirasa("Érvénytelen típus. Add meg újra!");
                 }
             }
 
             varos.EpuletHozzaad(new Epulet(cim, tipus));
-            Console.WriteLine("Épület hozzáadva. Nyomj egy gombot a folytatáshoz.");
+            SikerKiirasa("Épület hozzáadva. Nyomj egy gombot a folytatáshoz.");
             Console.ReadKey();
         }
 
         static void TuzoltoHozzaad(Varos varos)
         {
-            Console.WriteLine("--------------------------------------------------\n");
+            Console.Clear();
+            KirajzolFejlec("=== Tűzoltó Hozzáadása ===");
             Console.Write("Tűzoltó neve: ");
             string nev = Console.ReadLine()!;
             varos.TuzoltoHozzaad(new Tuzolto(nev));
-            Console.WriteLine("Tűzoltó hozzáadva. Nyomj egy gombot a folytatáshoz.");
+            SikerKiirasa("Tűzoltó hozzáadva. Nyomj egy gombot a folytatáshoz.");
             Console.ReadKey();
         }
 
         static void TuzoltoautoHozzaad(Varos varos)
         {
+            Console.Clear();
+            KirajzolFejlec("=== Tűzoltóautó Hozzáadása ===");
             string rendszam;
             while (true)
             {
-                Console.WriteLine("--------------------------------------------------\n");
                 Console.Write("Tűzoltóautó rendszáma (Formátum: ABC123): ");
                 rendszam = Console.ReadLine()!.ToUpper();
                 if (Regex.IsMatch(rendszam, @"^[A-Z]{3}\d{3}$"))
-                {
                     break;
-                }
-                Console.WriteLine("Érvénytelen formátum! Használj 3 betűt és 3 számot (pl. ABC123).");
+
+                HibaKiirasa("Érvénytelen formátum! Használj 3 betűt és 3 számot (pl. ABC123).");
             }
 
             varos.TuzoltoautoHozzaad(new Tuzoltoauto(rendszam));
-            Console.WriteLine("Tűzoltóautó hozzáadva. Nyomj egy gombot a folytatáshoz.");
+            SikerKiirasa("Tűzoltóautó hozzáadva. Nyomj egy gombot a folytatáshoz.");
             Console.ReadKey();
         }
 
         static void VizforrasHozzaad(Varos varos)
         {
-            Console.WriteLine("--------------------------------------------------\n");
+            Console.Clear();
+            KirajzolFejlec("=== Vízkút Hozzáadása ===");
             Console.Write("Vízkút helye: ");
             string hely = Console.ReadLine()!;
             int kapacitas;
@@ -134,127 +153,43 @@ namespace TuzoltosagSim
             {
                 Console.Write("Vízkút kapacitása: ");
                 if (int.TryParse(Console.ReadLine(), out kapacitas) && kapacitas > 0)
-                {
                     break;
-                }
-                Console.WriteLine("Érvénytelen kapacitás. Add meg újra!");
+
+                HibaKiirasa("Érvénytelen kapacitás. Add meg újra!");
             }
 
             varos.VizforrasHozzaad(new Vizforras(hely, kapacitas));
-            Console.WriteLine("Vízkút hozzáadva. Nyomj egy gombot a folytatáshoz.");
-            Console.ReadKey();
-        }
-
-        static void TuzInditas(Varos varos)
-        {
-            Console.WriteLine("--------------------------------------------------\n");
-            if (varos.Epuletek.Count == 0)
-            {
-                Console.WriteLine("Nincs épület hozzáadva. Nyomj egy gombot a folytatáshoz.");
-                Console.ReadKey();
-                return;
-            }
-
-            Console.WriteLine("Válassz egy épületet a tűz indításához:");
-            for (int i = 0; i < varos.Epuletek.Count; i++)
-            {
-                Console.WriteLine($"{i + 1}. {varos.Epuletek[i].Cim}");
-            }
-
-            int epuletIndex;
-            while (true)
-            {
-                if (int.TryParse(Console.ReadLine(), out epuletIndex) && epuletIndex > 0 && epuletIndex <= varos.Epuletek.Count)
-                {
-                    epuletIndex--;
-                    break;
-                }
-                Console.WriteLine("Érvénytelen választás. Add meg újra!");
-            }
-
-            TuzTipus tuzTipus;
-            while (true)
-            {
-                Console.WriteLine("Tűz típusa (1: Közönséges, 2: Olaj, 3: Elektromos): ");
-                string tuzTipusValasztas = Console.ReadLine()!;
-                try
-                {
-                    tuzTipus = tuzTipusValasztas switch
-                    {
-                        "1" => TuzTipus.Kozonseges,
-                        "2" => TuzTipus.Olaj,
-                        "3" => TuzTipus.Elektromos,
-                        _ => throw new Exception("Érvénytelen típus.")
-                    };
-                    break;
-                }
-                catch (Exception)
-                {
-                    Console.WriteLine("Érvénytelen típus. Add meg újra!");
-                }
-            }
-
-            varos.Epuletek[epuletIndex].TuzKiindul(tuzTipus);
-            Console.WriteLine($"Tűz indítva a(z) {varos.Epuletek[epuletIndex].Cim} címen. Nyomj egy gombot a folytatáshoz.");
-            Console.ReadKey();
-        }
-
-        static void TuzOltas(Varos varos)
-        {
-            Console.WriteLine("--------------------------------------------------\n");
-            if (varos.Epuletek.Count == 0)
-            {
-                Console.WriteLine("Nincs épület hozzáadva. Nyomj egy gombot a folytatáshoz.");
-                Console.ReadKey();
-                return;
-            }
-
-            Console.WriteLine("Válassz egy épületet a tűz oltásához:");
-            for (int i = 0; i < varos.Epuletek.Count; i++)
-            {
-                Console.WriteLine($"{i + 1}. {varos.Epuletek[i].Cim} (Tűz: {(varos.Epuletek[i].TuzVan ? "Van" : "Nincs")})");
-            }
-
-            int epuletIndex;
-            while (true)
-            {
-                if (int.TryParse(Console.ReadLine(), out epuletIndex) && epuletIndex > 0 && epuletIndex <= varos.Epuletek.Count)
-                {
-                    epuletIndex--;
-                    break;
-                }
-                Console.WriteLine("Érvénytelen választás. Add meg újra!");
-            }
-
-            try
-            {
-                int felhasznaltViz = varos.Tuzoltas(varos.Epuletek[epuletIndex]);
-                Console.WriteLine($"Tűz sikeresen eloltva a(z) {varos.Epuletek[epuletIndex].Cim} címen. Felhasznált víz: {felhasznaltViz} liter.");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Hiba történt: {ex.Message}");
-            }
-
-            Console.WriteLine("Nyomj egy gombot a folytatáshoz.");
+            SikerKiirasa("Vízkút hozzáadva. Nyomj egy gombot a folytatáshoz.");
             Console.ReadKey();
         }
 
         static void SzimulacioInditasa(Varos varos)
         {
+            Console.Clear();
+            KirajzolFejlec("=== Szimuláció Indítása ===");
             if (varos.Epuletek.Count == 0)
             {
-                Console.WriteLine("Nincs épület a szimulációhoz. Nyomj egy gombot a folytatáshoz.");
+                HibaKiirasa("Nincs épület a szimulációhoz. Nyomj egy gombot a folytatáshoz.");
                 Console.ReadKey();
                 return;
             }
 
             Random rnd = new Random();
             List<Epulet> tuzesEpuletek = new List<Epulet>();
+            List<Epulet> leegtek = new List<Epulet>();
 
             foreach (var epulet in varos.Epuletek)
             {
-                if (rnd.NextDouble() < 0.4 && !epulet.TuzVan)
+                if (epulet.TuzVan)
+                {
+                    if (rnd.NextDouble() < 0.5)
+                    {
+                        varos.EpuletElveszt(epulet);
+                        leegtek.Add(epulet);
+                        HibaKiirasa($"A(z) {epulet.Cim} címen lévő épület leégett!");
+                    }
+                }
+                else if (rnd.NextDouble() < 0.4)
                 {
                     try
                     {
@@ -264,7 +199,7 @@ namespace TuzoltosagSim
                     }
                     catch (Exception ex)
                     {
-                        Console.WriteLine($"Hiba a(z) {epulet.Cim} épületnél: {ex.Message}");
+                        HibaKiirasa($"Hiba a(z) {epulet.Cim} épületnél: {ex.Message}");
                     }
                 }
             }
@@ -281,6 +216,15 @@ namespace TuzoltosagSim
             else
             {
                 Console.WriteLine("Egyetlen épületben sem történt tűz.");
+            }
+
+            if (leegtek.Count > 0)
+            {
+                Console.WriteLine("\nLeégett épületek:");
+                foreach (var epulet in leegtek)
+                {
+                    Console.WriteLine($"- {epulet.Cim}");
+                }
             }
 
             Console.WriteLine("\nNyomj egy gombot a folytatáshoz.");
